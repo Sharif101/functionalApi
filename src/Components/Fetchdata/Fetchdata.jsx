431e996ae740c86e77af2e5a8be4ddfc3fab71f3
Sharif101 <sharif009009.com>
2023-09-13 16:01:43 +0600

    add

---
+++ src/Components/Fetchdata/Fetchdata.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import classes from "./Fetchdata.module.css";
+
+export default function Fetchdata() {
+  const [data, setData] = useState([]);
+
+  console.log(data);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/Sharif101")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  return (
+    <div className={classes.info}>
+      <div>
+        <img src={data.avatar_url} alt="" />
+        <h1>Name: {data.name}</h1>
+        <p>Company name: {data.company}</p>
+        <p>location: {data.location}</p>
+      </div>
+    </div>
+  );
+}
