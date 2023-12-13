1f9ffb43cc126af977b28e1cd069b59e7f4dd2a2
Sharif101 <sharif009009@gmail.com>
2023-12-13 09:38:01 +0600

    check

--- src/Components/TotalAppoin/TotalAppoin.jsx
+++ src/Components/TotalAppoin/TotalAppoin.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const TotalAppoin = () => {
-  return <div>Total Appoinmnet</div>;
+  return (
+    <div>
+      Total Appoinmnet
+      <p>for check git repo</p>
+    </div>
+  );
 };
 
 export default TotalAppoin;
