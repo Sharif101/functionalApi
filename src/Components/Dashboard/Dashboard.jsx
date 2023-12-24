dca332431e11cee9d72001554d793fb2fb31546e
Sharif101 <sharif009009@gmail.com>
2023-12-24 14:54:00 +0600

    add dashboard

---
+++ src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./Dashboard.module.css";
+import Sidebar from "./Sidebar/Sidebar";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h5>Dashboard</h5>
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/Components/Dashboard/Sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import data from "../../Data/Sidebardata.json";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <p>side nav</p>
+    </div>
+  );
+};
+
+export default Sidebar;
